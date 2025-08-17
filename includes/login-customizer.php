e92d7e852b9dd8783641cc5993391d2234b027b3
Irish Egie Arao <irishegie.arao@ssu.edu.ph>
2025-08-17 10:05:01 +0700

    update: changed login customizer to use the pinabacdao logo

--- includes/login-customizer.php
+++ includes/login-customizer.php
@@ -2,7 +2,18 @@
 
 function my_custom_login_logo() {
     $logo_url = get_stylesheet_directory_uri() . '/assets/images/logo.png';
-    
+    echo '
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(' . $logo_url . ');
+            height: 80px;
+            width: 300px;
+            background-size: contain;
+            background-repeat: no-repeat;
+            padding-bottom: 30px;
+        }
+    </style>
+    ';
 }
 add_action('login_enqueue_scripts', 'my_custom_login_logo');
 
